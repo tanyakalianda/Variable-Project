276f873daa6bb2bc7ad8049bb08795d55e2a39af
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-09-13 15:14:35 -0400

    Add files via upload

---
+++ src/src/VariableProject.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author tkalianda19
+ * Variable Project
+ */
+public class VariableProject 
+{
+
+	public static void main(String[] args) {
+		String name = "My name is Tanya";
+		System.out.println(name);
+		
+	int length = 4;
+	int width = 3;
+	int area = length*width;
+	System.out.println(area);
+	
+	int radius = 3;
+	double pi = 3.14;
+	double circlearea = radius*radius*pi;
+	System.out.println(circlearea);
+	double circumference = 2*pi*radius;
+	System.out.println(circumference);
+	}
+
+}
